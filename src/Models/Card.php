1ca4800e3279814da49e64f5cb003cc08d196c40
Linn <lich23@student.bth.se>
2024-05-02 13:55:35 +0200

    uppdatera

--- src/Models/Card.php
+++ src/Models/Card.php
@@ -1,24 +1,18 @@
 <?php
 
 // card/deck/shuffle som visar samtliga kort i kortleken när den har blandats.
+
 namespace App\Models;
 
 class Card
 {
     protected $value;
     protected $suit;
-    
+
 
     public function __construct()
     {
 
-    }
-    
-
-    public function shuffle(): int
-    {
-        $this->value = random_int(1, 46);
-        return $this->value;
     }
 
     public function getAsString(): string
