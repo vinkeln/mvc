75d1edee8c88f14474dd9e62d81add8fcad1d4b7
Linn <lich23@student.bth.se>
2024-05-02 13:56:59 +0200

    uppdatera

--- src/Models/CardGraphic.php
+++ src/Models/CardGraphic.php
@@ -4,7 +4,6 @@ namespace App\Models;
 
 //skapar en ny class och ärver från Cards-klassen
 class CardGraphic extends Card
-
 { //skapar en privat variabel som innehåller en array med kort
     //kan bara nås inifrån CardGraphic-klassen
     private $representation = [
@@ -21,26 +20,32 @@ class CardGraphic extends Card
     {
         parent::__construct();
     }
-    
-    public function drawCard($number = 1)
-{
-    if (!is_array($this->representation)) {
-        throw new \Exception("Expected representation to be an array.");
+
+
+    public function shuffle()
+    {
+        return shuffle($this->representation);
     }
-    $cardsDrawn = array_splice($this->representation, 0, $number);
-    return $cardsDrawn;
-}
+    public function drawCard($number = 1)
+    {
+        if (!is_array($this->representation)) {
+            throw new \Exception("Expected representation to be an array.");
+        }
+        $cardsDrawn = array_splice($this->representation, 0, $number);
+        return $cardsDrawn;
+    }
 
     public function shuffleCards()
     {
         shuffle($this->representation);
     }
+    
 
-    public function remainingCards()
+    /*public function remainingCards()
     {
         return count($this->representation);
-        
-    }
+
+    }*/
 
     public function getCards(): array
     {
@@ -53,15 +58,15 @@ class CardGraphic extends Card
     {
         return $this->representation[$this->value - 1];
     }
-    
-    
-    public function getDeck()
-{
-    return $this->representation;
-}
 
-    public function setDeck($deck) {
-        $this->representation = $deck;
+    //flytta till CardDeck
+    public function getDeck()
+    {
+        return $this->representation;
     }
 
+    public function setDeck($deck)
+    {
+        $this->representation = $deck;
+    }
 }
